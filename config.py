1c1c148233d502daa93f568e7a902c4d9d2ea655
Bharadwaj Subramanian <subramanian.bharadwaj@gmail.com>
2015-04-27 22:38:48 -0700

    added cleaned config

---
+++ config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': '',
+    'topics': ['#nepalquake', '#nepalquakerelief', '#nepalearthquake',
+               '#nepalquakerescue', '#nepalrescue', '#act4quake', 
+               '#helpnepal']
+}
