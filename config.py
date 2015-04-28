5539959495cdf3b7ff9e2694ff3c0b34b2fdfd46
Bharadwaj Subramanian <subramanian.bharadwaj@gmail.com>
2015-04-27 22:35:30 -0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-CONFIG = {
-    'consumer_key': 'cYXGV9nOPdqazN0XIJuYbvhsz',
-    'consumer_secret': 'oIR8w3UKvSKkS3Z2qlmih4Wv7CTsRbSvCqUtJEBKEucKD9Pg1H',
-    'access_token': '388120934-34snaHbC0LHZLQu6UipO1o8XaGUvJYjBFLXXpV9y',
-    'access_token_secret': 'j3QnziLVaZDKJYTCPuAS9Y9yxyRowoHavfIVCncVUZO1N',
-    'topics': ['#nepalquake', '#nepalquakerelief', '#nepalearthquake',
-               '#nepalquakerescue', '#nepalrescue', '#act4quake', 
-               '#helpnepal']
-}
