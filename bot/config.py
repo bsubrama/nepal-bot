ab6c68ad9857b820ec5a6aac22152f403146ca65
Bharadwaj Subramanian <subramanian.bharadwaj@gmail.com>
2015-04-29 00:48:14 -0700

    more exclude/include

--- bot/config.py
+++ bot/config.py
@@ -9,8 +9,11 @@ CONFIG = {
     'excluded_keywords': ['donate', 'report', 'follow', 'damage', 'organization',
                           'say', 'thoughts', 'prayers', 'efforts', 'learn',
                           'rescued', 'recovered', 'evacuated', 'increase', 'rise',
-                          'death toll', 'recovery', 'bjp', 'tells', 'prayer'],
+                          'death toll', 'recovery', 'bjp', 'tells', 'prayer', 'proud',
+                          'struggles', 'solidarity'],
     'keywords': ['food','tent','water','medicine','danger','urgent',
                  'emergency','assist','necessary','medicines','rescue',
-                 'need','clothe','clothes','volunteer','relief','meds','aid']
+                 'need','clothe','clothes','volunteer','relief','meds','aid', 'seeking',
+                 'hospital', 'immediate', 'sindhupalchowk', 'sindhupalchok', 'kathmandu',
+                 ]
 }
